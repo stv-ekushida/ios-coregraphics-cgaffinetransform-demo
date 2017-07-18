582dd8dfb1b9f60e35ebfed3a86a37c6f2d8b423
sdt-kushida <e.kushida@st-ventures.jp>
2017-07-19 08:05:17 +0900

    ios-coregraphics-cgaffinetransform-demo

---
+++ ios-coregraphics-cgaffinetransform-demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ios-coregraphics-cgaffinetransform-demo
+//
+//  Created by Eiji Kushida on 2017/07/19.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateTranslate()
+    }
+
+    private func updateTranslate() {
+
+        let rotationAngleTransform = CGAffineTransform(rotationAngle: 45)
+        let scaleTransform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        let transform = rotationAngleTransform.concatenating(scaleTransform)
+        imageView.transform = transform
+    }
+}
+
